0b5d0e57b073df8c2fe237aadd5ed1722f063b26
Logan Hartman <loganchart@gmail.com>
2023-02-06 16:18:59 -0500

    working on form

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,12 +1,25 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
+
 
 const LoginForm = () => {
 
     const [credential, setCredential ] = useState ('')
     const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(false);
+    
+    const toggleShowPassword= (e) => {
+        e.prevent.default()
+        setShowPassword(showPassword ? false : true);
+    };
+
+    const handleSubmit = (e) => {
+        e.prevent.default()
+        console.log(e)
+    }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Login to your account</h1>
         <label>Username or email
             <input 
@@ -18,6 +31,23 @@ const LoginForm = () => {
                 }
             />
         </label>
+        <br/>
+        <label>Password
+            <input 
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => {
+                    console.log(password)
+                    setPassword(e.target.value)}
+                }
+            />
+        </label>
+        <br/>
+        <button onClick={(e) => toggleShowPassword(e)}>Show</button>
+        <br/>
+        <button type="Submit">Login</button>
+        <br/>
+        <Link to="/">Forgot your password?</Link>
 
         </form>
     )
